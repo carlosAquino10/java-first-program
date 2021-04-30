4bd1f11163c5760b5dfe793a601f4f0c728ca09e
carlosAquino10 <83220014+carlosAquino10@users.noreply.github.com>
2021-04-30 11:51:41 -0300

    teste

---
+++ src/main/java/com/h2/Utilities.java
@@ -0,0 +1,34 @@
+package  com.h2 ;
+
+public  class  Utilities {
+
+    public  static  long  getLongValue ( String  in ) {
+        long out =  Long . MIN_VALUE ;
+        tente {
+            out =  Long . parseLong (em);
+        } catch ( NumberFormatException e) {
+            lançar  new  IllegalArgumentException (in +  " não pode ser convertido em um valor 'longo'. Saindo do programa. " );
+        }
+        voltar para fora;
+    }
+
+    public  static  int  getIntValue ( String  in ) {
+        int out =  inteiro . MIN_VALUE ;
+        tente {
+            out =  Inteiro . parseInt (em);
+        } catch ( NumberFormatException e) {
+            lançar  new  IllegalArgumentException (in +  " não pode ser convertido em um valor 'int'. Saindo do programa. " );
+        }
+        voltar para fora;
+    }
+
+    public  static  float  getFloatValue ( String  in ) {
+        flutuar para fora =  flutuar . MIN_VALUE ;
+        tente {
+            out =  Float . parseFloat (em);
+        } catch ( NumberFormatException e) {
+            lançar  new  IllegalArgumentException (in +  " não pode ser convertido em um valor 'flutuante'. Saindo do programa. " );
+        }
+        voltar para fora;
+    }
+}
